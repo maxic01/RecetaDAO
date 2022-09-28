771f81ff12461db87f2e3d8189f4d4fdae962238
maxic01 <maxi1998calvo@gmail.com>
2022-09-27 21:47:50 -0300

    Agregar archivos de proyecto.

---
+++ RecetasSLN/datos/GestorDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using RecetasSLN.dominio;
+
+namespace RecetasSLN.datos
+{
+    internal class GestorDB
+    {
+        private IRecetaDao RDao;
+        public DataTable listarIngredientes()
+        {
+            RDao = new RecetaDAO();
+            return RDao.listarIngredientes();
+        }
+        public int proximaReceta()
+        {
+            RDao = new RecetaDAO();
+            return RDao.proximaReceta();
+        }
+        public bool ejecutarSP(Receta oReceta)
+        {
+            RDao = new RecetaDAO();
+            return RDao.ejecutarSP(oReceta);
+        }
+    }
+}
